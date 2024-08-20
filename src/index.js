7d3acd3185e7c0b1cedd06ef4af1d8cda996f77e
BMOP98 <133816035+BMOP98@users.noreply.github.com>
2024-08-20 15:25:46 -0500

    3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
